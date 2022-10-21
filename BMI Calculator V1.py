eeb7f0891ae05fa22239397fa1df485f89af26ca
Observer <80362048+68756d616e@users.noreply.github.com>
2022-10-21 21:04:22 +0100

    Add files via upload

---
+++ BMI Calculator V1.py
@@ -0,0 +1,16 @@
+# BMI Calculator basic
+# How it works! You input your Height and Weight and you will receive a BMI result.
+# The process : Weight / Height & then height squared by itself
+# Example 70kg divided by 183m squared
+
+Weight = input("Please type your weight in kg: ")
+Height = input("Please type your hieght in m: ")
+
+BMI = (int(Weight) / float(Height) ** 2)
+BMI_INT = (int(BMI))
+
+print(BMI_INT)
+
+
+
+
